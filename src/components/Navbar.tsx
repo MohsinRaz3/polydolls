7a33b89b8b27fcde243b5e54d64397aebbc06f33
mohsinraz3 <mohsinraz3@gmail.com>
2023-10-15 23:49:51 +0500

    initialized

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='bg-white  mx-auto flex w-full items-center justify-center p-3 '>
+            <h2 className='text-black'>Pollydolls</h2>
+        </div>
+    )
+}
+
+export default Navbar
