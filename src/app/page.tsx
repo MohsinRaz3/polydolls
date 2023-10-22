277f3d62a38e248911399b74a03afff377a8e964
mohsinraz3 <mohsinraz3@gmail.com>
2023-10-22 15:42:24 +0500

    banner bg/tittle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,10 +41,10 @@ export default function Home() {
               <div className='w-[12/12] h-full md:pl-12 sm:mt-[-3rem] ml:mt-[-4rem] md:mt-[-2rem] lg:mt-[-0.2rem] xl:mt-[-1rem] '>   <motion.h1
                 className=" tracking-[-0.02em] drop-shadow-sm md:leading-[5rem]"
                 variants={FADE_DOWN_ANIMATION_VARIANTS}
-              >  <Image src={"/logo.png"} height={1000} width={500} className='mt-36 md:mt-8 md:w-[650px]' alt='polydolls logo Nfts'></Image> </motion.h1>
+              >  <Image src={"/logo.png"} height={1000} width={500} className='mt-36 md:mt-2 md:w-[700px]' alt='polydolls logo Nfts'></Image> </motion.h1>
               </div>              {/* <h1 className={`text-white ${silkScreen.className} text-3xl md:text-[70px] lg:md:text-[86px]  textshadow`}>Polydolls LOGO</h1> */}
               {/* <h3 className='text-white text-lg md:text-2xl md:font-medium w-11/12 text-center mx-auto my-5 md:my-10'>30,000 Polygon citizens, eager for their ancestral nest. Mint now to pave their voyage home and earn commercial rights.</h3> */}
-              <Link className='w-9/12 mm:w-6/12 md:w-[200px] m-auto  ' href={"https://mint.polydolls.com"} target='_blank'><button className='w-full  bg-[#312040] text-white my-12 md:ml-6  md:my-12 md:mt-13 lg:mt-[30px] md:mb-28 font-semibold  border-black  py-2 md:py-3 lg:py-4 full rounded-md  custom-gradient'> Mint Now</button></Link>
+              <Link className='w-9/12 mm:w-6/12 md:w-[200px] m-auto  ' href={"https://mint.polydolls.com"} target='_blank'><button className='w-full  bg-[#312040] text-white my-12 md:ml-6  md:my-12 md:mt-8 lg:mt-[20px] md:mb-28 font-semibold  border-black  py-2 md:py-3 lg:py-4 full rounded-md  custom-gradient'> Mint Now</button></Link>
             </div>
 
             {/* <div className='h-72 lg:h-[500px]  w-full  bg-[url(/Banner1.png)] bg-no-repeat bg-cover bg-center'></div> */}
